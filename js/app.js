88e3e9f2cd86a1a105b0e8e0dcb5b5f7515ffd19
Jobe Paul Authement <jobeauthement@gmail.com>
2023-03-30 00:06:02 -0500

    First Commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Questios section animations start here
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
+// Questios section animations end here
